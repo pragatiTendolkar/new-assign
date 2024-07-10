37d6f078f46037fe195072a25765a3b9d3b6af27
Pragati Tendolkar <95703908+pragatiTendolkar@users.noreply.github.com>
2024-07-11 00:02:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+  let currentScroll = 0;
+let isScrollingDown = true;
+let arrows = document.querySelectorAll(".arrow");
+
+let tween = gsap.to(".marquee-text", {
+  xPercent: -100,
+  repeat: -1,
+  duration: 5,
+  ease: "linear"
+}).totalProgress(0.5);
+
+gsap.set(".marquee-box", { xPercent: 0 });
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > currentScroll) {
+    isScrollingDown = true;
+  } else {
+    isScrollingDown = false;
+  }
+  gsap.to(tween, {
+    timeScale: isScrollingDown ? 1 : -1
+  });
+
+  arrows.forEach((arrow) => {
+    if (isScrollingDown) {
+      arrow.classList.remove("active");
+    } else {
+      arrow.classList.add("active");
+    }
+  });
+
+  currentScroll = window.pageYOffset;
+});
